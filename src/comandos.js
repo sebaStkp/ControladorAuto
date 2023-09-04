d6a54dea7c21aa7779bf13505fb10b878dc7f7a2
Sebastian Terrazas Kippes <sebastian.terrazas@ucb.edu.bo>
2023-09-03 23:04:26 -0400

    Solo permite tipo de dato cadena para los comandos

--- src/comandos.js
+++ src/comandos.js
@@ -1,4 +1,20 @@
+function verificarFormatoAuto(comandos){
+    var result = false;
+    if(typeof comandos == "string"){
+        result = true;
+    }
+    return result;
+}
+
+
 function comsAuto(comandos){
-    return comandos;
+    var coms;
+    if(verificarFormatoAuto(comandos)){
+        coms = comandos;
+    }
+    else{
+        coms = "Formato incorrecto";
+    }
+    return coms;
 }
 export default comsAuto;
--- src/comandos.test.js
+++ src/comandos.test.js
@@ -4,4 +4,8 @@ describe("Comandos para mover el auto", () => {
     it("Deberia mostrar los comandos para el movimiento del auto(Avanzar, Derecha, Izquierda)", () => {
       expect(comsAuto("ADI")).toEqual("ADI");
     });
+    it("Solo deja ingresar elementos tipo cadena", () => {
+        expect(comsAuto(45)).toEqual("Formato incorrecto");
+    });
+    
   });
